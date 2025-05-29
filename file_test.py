2d3047374fb7826a56a9b2a4773ec00ef0ba8dfb
Camille <lescanne.camille@gmail.com>
2025-05-29 08:43:21 +0000

    Correction style de file_test.py

--- file_test.py
+++ file_test.py
@@ -1,21 +1,22 @@
-import pytest
-
 def test_cald_addition():
-    #fonction test du resultat de 2 + 4
-    output = 2+4
+    # fonction test du resultat de 2 + 4
+    output = 2 + 4
     assert output == 6
 
+
 def test_calc_sustraction():
-    #fonction test du résultat de 2 - 4
-    output = 2-4
+    # fonction test du résultat de 2 - 4
+    output = 2- 4
     assert output == -2
 
+
 def test_calc_multiplication():
-    #fonction test du résultat de 2*4
-    output = 2*4
+    # fonction test du résultat de 2*4
+    output = 2 * 4
     assert output == 8
 
+
 def test_coucou():
-    #fonction test si le résultat est 'hello'
+    # fonction test si le résultat est 'hello'
     output = 'hello'
     assert output == 'hello'
